b9f6a9905eaa78568232a07ce361c2523c8e9b75
RHorn2006 <ryan99horn@gmail.com>
2023-12-03 00:08:39 +0000

    Added calculation and input file, still need to link together tho and sort out regex

---
+++ src/Day 1/AOC_DAY1/AOC_DAY1/Main.cs
@@ -0,0 +1,44 @@
+﻿using System.Text.RegularExpressions;
+
+class Program
+{
+    static void Main()
+    {
+        List<string> inputStrings = new List<string>
+        {
+            "abcdef6ghi9",
+            "xyz767pqr4mno1",
+            "textNoN6umbers",
+            "multiple2numbers57inBetween"
+        };
+
+        int totalValue = TotalValueCalc(inputStrings);
+        Console.WriteLine($"this is the {totalValue}");
+    }
+
+    static int TotalValueCalc(List<string> inputStrings)
+    {
+        int totalValue = 0;
+
+        Regex regex = new Regex(@"^\D*(\d).*?(\d)\D*$"); //sort out tomorrow
+
+        foreach (string inputString in inputStrings)
+        {
+            MatchCollection matches = regex.Matches(inputString);
+
+            if (matches.Count > 0)
+            {
+                int firstNumber = int.Parse(matches[0].Value);
+                int lastNumber = int.Parse(matches[matches.Count - 1].Value);
+
+                int mergedNumber = int.Parse($"{firstNumber}{lastNumber}");
+
+                Console.WriteLine(mergedNumber);
+
+                totalValue += mergedNumber;
+            }
+        }
+        
+        return totalValue;
+    }
+}
